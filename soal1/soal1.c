b6b762098baedf8cd19bec1e8ee7657773403ba1
haritssm <38557477+haritssm@users.noreply.github.com>
2019-03-23 16:31:22 +0700

    Update soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -38,43 +38,24 @@ int main() {
   close(STDOUT_FILENO);
   close(STDERR_FILENO);
 
-	FILE *f;
-	char str[100];
   while(1) {
-	/*DIR *dir;
-    	struct dirent *file;
-    	dir = opendir("/home/zicoritonda/sisop/Modul2/SoalShiftModul2/soal1");
-    	if (dir != NULL){
-       		while ((file = readdir(dir)) != NULL){
-          		if ((strstr(file->d_name,".txt")) != NULL){
-            			char current[256];
-				strcpy(current,"/home/zicoritonda/sisop/Modul2/SoalShiftModul2/soal1");
-                 		char target[256];
-				strcpy(target,"/home/zicoritonda/modul2/gambar");
-                 		char filename[256];
-            			strcpy(filename, file->d_name, strlen(file->d_name)-4);
-            			strcat(filename, "_grey.txt");
-				strcat(current,file->d_name);
-				strcat(target,filename);
-            			rename(file->d_name, filename);
-          		}
-        	}
-    	}
-    	closedir(dir);*/
-	char current[256] = "/home/zicoritonda/sisop/Modul2/SoalShiftModul2/soal1";
-	char target[256] = "/home/zicoritonda/modul2/gambar";
-	char name[100];
-	char filename[100];
-	f = popen("find . -name '*.txt' -printf '%f\n'","r");
-	while(fgets(str,sizeof(str),f) != NULL){
-		strncpy(name,str,strlen(str)-4);
-		sprintf(filename,"%s_grey.txt",name);
-		strcat(current,str);
-		strcat(target,filename);
-		rename(str,filename);
-	}
-	pclose(f);
-    	sleep(10);
+    DIR *dir;
+    struct dirent *file;
+    dir = opendir("/home/haritssm/sisop/modul2/soal1/");
+    if (dir != NULL){
+       while (file = readdir(dir)){
+          if (strstr(&file->d_name[strlen(file->d_name)-5], ".png")) {
+            char current[256] = "/home/haritssm/sisop/modul2/soal1/",
+                 target[256] = "/home/haritssm/sisop/modul2/soal1/gambar/",
+                 filename[256] = "";
+            strncpy(filename, file->d_name, strlen(file->d_name)-4);
+            strcat(filename, "_grey.png");
+            rename(strcat(current, file->d_name), strcat(target, filename));
+          }
+        }
+    }
+    closedir(dir);
+    sleep(10);
   }
   
   exit(EXIT_SUCCESS);
